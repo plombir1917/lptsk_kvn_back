bf4ea461ce487c27fdd2032dd09049c2984576a5
plombir1917 <dfhhggh@list.ru>
2024-05-12 19:02:39 +0300

    refactor: main.ts, app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/database/prisma.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
@@ -16,8 +15,7 @@ import { PrismaModule } from 'src/database/prisma.module';
       autoSchemaFile: true,
     }),
     PrismaModule,
+    AccountModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
