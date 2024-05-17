91979691c4496fbee4fe52d51119c28d8352163b
plombir1917 <dfhhggh@list.ru>
2024-05-17 22:45:08 +0300

    feat: jwt module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/database/prisma.service';
+import { AuthResolver } from './auth.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      global: true,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, AuthResolver],
 })
 export class AuthModule {}
