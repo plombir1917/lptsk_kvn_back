89897d2c01bd1fc076d6ec174da0202f8fccb299
KopichZS <dfhhggh@list.ru>
2024-05-17 14:45:26 +0300

    feat: bcrypt

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,13 @@
+import * as bcrypt from 'bcrypt';
+
+export async function encodePassword(rawPassword: string) {
+  const SALT = await bcrypt.genSalt();
+  return await bcrypt.hash(rawPassword, SALT);
+}
+
+export async function comparePassword(
+  rawPassword: string,
+  encodedPassword: string,
+) {
+  return await bcrypt.compare(rawPassword, encodedPassword);
+}
