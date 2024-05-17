ed25a6c41e3dc4dc0f3a80b3e65f3c5a6e0d3719
KopichZS <dfhhggh@list.ru>
2024-05-17 15:10:17 +0300

    refactor: error message

--- src/errors/account-exist.error.ts
+++ src/errors/account-exist.error.ts
@@ -1,5 +1,5 @@
 export class AccountExistError extends Error {
   constructor(message: string) {
-    super(`Account with login: ${message} already exists!`);
+    super(`Account with login: '${message}' already exists!`);
   }
 }
