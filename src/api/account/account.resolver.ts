18f7a90daa104153473fd2d41f6bf578dda89998
KopichZS <dfhhggh@list.ru>
2024-05-17 13:56:40 +0300

    feat: createAccount, getAccountById methods; validation pipe; service inject

--- src/api/account/account.resolver.ts
+++ src/api/account/account.resolver.ts
@@ -1,16 +1,41 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from 'src/database/prisma.service';
 import { Account } from './account.entity';
+import { CreateAccountInput } from './dto/create-account.input';
+import { AccountService } from './account.service';
+import {
+  BadRequestException,
+  NotFoundException,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Resolver('Account')
 export class AccountResolver {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly accountService: AccountService) {}
 
+  @UsePipes(new ValidationPipe())
   @Mutation(() => Account)
-  async createAccount() {}
+  async createAccount(@Args('input') input: CreateAccountInput) {
+    try {
+      return await this.accountService.createAccount(input);
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException('Invalid data!');
+    }
+  }
 
-  @Query(() => String)
+  @Query(() => Account)
+  async getAccountById(@Args('id') id: string) {
+    try {
+      return await this.accountService.getAccountById(id);
+    } catch (error) {
+      throw new NotFoundException('Account not found!');
+    }
+  }
+
+  @Query(() => [Account])
   async getAccounts() {
-    return 'оно живое';
+    return await this.accountService.getAccounts();
   }
 }
