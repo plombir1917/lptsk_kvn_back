b0cb683f111a68db8d462cfa2b7b13524d3db7fb
KopichZS <dfhhggh@list.ru>
2024-05-17 13:55:17 +0300

    feat: inject account service, prisma service into account.model

--- src/api/account/account.module.ts
+++ src/api/account/account.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountResolver } from './account.resolver';
 import { PrismaModule } from 'src/database/prisma.module';
+import { PrismaService } from 'src/database/prisma.service';
+import { AccountService } from './account.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AccountResolver],
+  providers: [AccountResolver, AccountService, PrismaService],
 })
 export class AccountModule {}
