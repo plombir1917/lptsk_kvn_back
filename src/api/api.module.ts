8f8dd0182086bb586f1e32b09d91fa8a6a3f5b2f
KopichZS <dfhhggh@list.ru>
2024-05-17 15:03:00 +0300

    feat: auth module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, AuthModule],
   controllers: [],
   providers: [],
 })
