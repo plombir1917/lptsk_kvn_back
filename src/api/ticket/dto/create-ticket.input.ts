f0cede0cc570404e5545ed2bfbd1e8d3d0931262
KopichZS <dfhhggh@list.ru>
2024-06-10 17:39:57 +0300

    feat: ticket entity, dto

--- src/api/ticket/dto/create-ticket.input.ts
+++ src/api/ticket/dto/create-ticket.input.ts
@@ -2,6 +2,9 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateTicketInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  link: string;
+
+  @Field(() => Int)
+  event_id: number;
 }
--- src/api/ticket/entities/ticket.entity.ts
+++ src/api/ticket/entities/ticket.entity.ts
@@ -2,6 +2,12 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Ticket {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  link: string;
+
+  @Field(() => Int)
+  event_id: number;
 }
