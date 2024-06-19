d5d1bd2b1c2bb8dc3450ec6d8f2ad0c49ab37a37
plombir1917 <dfhhggh@list.ru>
2024-06-19 16:24:32 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new RolesGuard(app.get(JwtService), app.get(Reflector)));
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  app.enableCors({ allowedHeaders: '*', preflightContinue: true });
+  app.enableCors({ origin: true });
   app.use(graphqlUploadExpress({ maxFileSize: 100000000, maxFiles: 10 }));
   await app.listen(process.env.APPLICATION_PORT);
 }
